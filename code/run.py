88ae1c5102a394421ff5b4e86dafcff0004f22a2
richard <1042651820@qq.com>
2017-07-24 17:35:25 +0800

    test VotingClassifier

--- code/run.py
+++ code/run.py
@@ -1,6 +1,4 @@
 import time
-start = time.clock()
-
 from pkg import preprocess
 from pkg import vectorizer
 from sklearn.model_selection import KFold
@@ -11,6 +9,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score
 
@@ -18,6 +17,7 @@ data = preprocess.readplz()
 x,y=preprocess.get(data)
 
 def cv_loop(model, name):
+    start = time.clock()
     model.fit(x,y)
     predict_y = model.predict(x)
     print('f1-score:')
@@ -33,41 +33,40 @@ def cv_loop(model, name):
     for train,test in kf.split(x):
 
         x_train,x_test,y_train,y_test = x[train],x[test],y[train],y[test]
-        print('fit--->', end=' ')
+        #print('fit--->', end=' ')
         model.fit(x_train,y_train)
 
-        print('test predit',end=' ')
+        #print('test predit',end=' ')
         predict_y = model.predict(x_test)
         t_score = f1_score(y_test,predict_y, average='weighted')
         avg_test_f1score += t_score
-        print('test t1 score',end=' ')
-        print(t_score, end='')
+        #print('test t1 score',end=' ')
+        #print(t_score, end='')
 
-        print('train predit',end=' ')
+        #print('train predit',end=' ')
         predict_y = model.predict(x_train)
         t_score = f1_score(y_train,predict_y, average='weighted')
         avg_train_f1score += t_score
-        print('train t1 score',end=' ')
-        print(t_score)
-
+        #print('train t1 score',end=' ')
+        #print(t_score)
 
     print(name)
     print('avg_test_f1score = ',end=' ')
     print(avg_test_f1score/SPLITS)
     print('avg_train_f1score = ',end=' ')
     print(avg_train_f1score/SPLITS)
+    elapsed = (time.clock() - start)
+    print("Time used:", elapsed)
     print("----------")
 
 if __name__ == '__main__':
 
+    #voting = VotingClassifier(estimators=[('ef', ExtraTreesClassifier()), ('rf', RandomForestClassifier())], voting='soft', weights=[2,2])
 
-    models = [RandomForestClassifier(), ExtraTreesClassifier(), GaussianNB(), KNeighborsClassifier(),
+    models = ['RandomForestClassifier(n_estimators=10), ExtraTreesClassifier(), GaussianNB(), KNeighborsClassifier(),
         DecisionTreeClassifier(), LogisticRegression(), SVC()]
     model_names = ['RandomForestClassifier()', 'ExtraTreesClassifier()', 'GaussianNB()', 'KNeighborsClassifier()', 
         'DecisionTreeClassifier()', 'LogisticRegression()', 'SVC()']
 
     for i in range(len(models)):
         cv_loop(models[i], model_names[i])
-
-    elapsed = (time.clock() - start)
-    print("Time used:", elapsed)
